991cc797eb0f52c79dbeb3d8d397b06f47ab46d9
ZhangYuHua <1457896625@qq.com>
2021-10-06 08:32:16 +0800

    init project

--- demo_c/main.cpp
+++ demo_c/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  demo_c
-//
-//  Created by David Zhang on 2021/10/6.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, huahua !\n";
     return 0;
 }
