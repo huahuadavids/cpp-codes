f8fa0b9d4511062668d549a9ee347aff001177be
ZhangYuHua <1457896625@qq.com>
2021-10-06 08:25:36 +0800

    Initial Commit

---
+++ demo_c/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  demo_c
+//
+//  Created by David Zhang on 2021/10/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
